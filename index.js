be81a6f427a6f8ca52291acff3117850ab069ad1
Michael Fuget <michaelfuget@gmail.com>
2021-02-14 23:43:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ const fi = (function() {
         do
         {
           let out = []
-          collapsed = false;
+          let collapsed = false;
           for(let i=0; i<array.length; i++)
           {
               if(Array.isArray(array[i]))
